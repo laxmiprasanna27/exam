cbdce80982ebe4f0ade77ca996014028252e98ee
laxmiprasanna27 <laxmiprasanna12244@gmail.com>
2025-12-03 10:32:56 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask,render_template,request
+app=Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('form.html')
+@app.route('/submit',methods=['POST'])
+def submit():
+    name=request.form['name']
+    rol=request.form['rol']
+    email=request.form['email']
+    year=request.form['year']
+    return render_template('result.html',name=name,rol=rol,email=email,year=year)
+if __name__=='__main__':
+    app.run(debug=True)
+    
+    
+    
