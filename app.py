1f3c47d05631a7d69234e84828c30e185efac687
laxmiprasanna27 <154531062+laxmiprasanna27@users.noreply.github.com>
2025-12-03 10:52:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
 from flask import Flask,render_template,request
-app=Flask(__name__)
+app= Flask(__name__)
 @app.route('/')
 def index():
     return render_template('form.html')
 @app.route('/submit',methods=['POST'])
 def submit():
-    name=request.form['name']
-    rol=request.form['rol']
+    username=request.form['username']
+    rollno=request.form['rollno']
     email=request.form['email']
     year=request.form['year']
-    return render_template('result.html',name=name,rol=rol,email=email,year=year)
+    return render_template('results.html',username=username,rollno=rollno,email=email,year=year)
 if __name__=='__main__':
     app.run(debug=True)
-    
-    
-    
